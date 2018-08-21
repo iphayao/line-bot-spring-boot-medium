34ae24c5ce2d49f22641381ae8fd80efe5ae2149
iphayao <phayaob@gmail.com>
2018-08-21 19:57:33 +0700

    Revert "Add Example Bubble Flex Message"
    This reverts commit 6abe72c98152cfac84fbdde6e1ca7812fa5af5b1.

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -1,7 +1,6 @@
 package com.iphayao.linebot;
 
 import com.google.common.io.ByteStreams;
-import com.iphayao.linebot.flex.BubbleFlexMessageSupplier;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.client.MessageContentResponse;
 import com.linecorp.bot.model.ReplyMessage;
@@ -157,10 +156,6 @@ public class LineBotController {
                     lineMessagingClient.linkRichMenuIdToUser(userId, homeMenu);
                     return;
                 }
-                break;
-            }
-            case "flex": {
-                this.reply(replyToken, new BubbleFlexMessageSupplier().get());
                 break;
             }
             default:
